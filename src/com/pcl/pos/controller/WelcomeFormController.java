7ee4ed7db8e8d9586a5b157cea10661bf8f93227
GIhanvs98 <darkcreationsl98@gmail.com>
2026-09-17 21:26:07 +0530

    feat(welcome-form):implemented login & signup navigation from welcome page

--- src/com/pcl/pos/controller/WelcomeFormController.java
+++ src/com/pcl/pos/controller/WelcomeFormController.java
@@ -1,17 +1,31 @@
 package com.pcl.pos.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class WelcomeFormController {
     public AnchorPane context;
 
-    public void loginFormOnAction(ActionEvent actionEvent) {
-        System.out.println("clicked login");
+    public void loginFormOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("LoginForm");
     }
 
-    public void signupFormOnAction(ActionEvent actionEvent){
-        System.out.println("clicked signup");
+    public void signupFormOnAction(ActionEvent actionEvent) throws IOException {
+     setUi("SignupForm");
+
+    }
+
+    public void setUi(String location) throws IOException {
+        Parent parent=FXMLLoader.load(getClass().getResource
+                ("/com/pcl/pos/view/"+location+".fxml"));
+        javafx.stage.Stage stage =(Stage)context.getScene().getWindow();
+        stage.setScene(new Scene(parent));
 
     }
 }
