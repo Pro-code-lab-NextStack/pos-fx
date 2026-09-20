41b21f7fc763dd308e0efb1a3f86eff9bf7e673c
GIhanvs98 <darkcreationsl98@gmail.com>
2026-09-20 22:19:55 +0530

    feat(registration):implemented user create feat and jdbc connection also updated module-info

--- src/com/pcl/pos/controller/SignUpFormController.java
+++ src/com/pcl/pos/controller/SignUpFormController.java
@@ -1,9 +1,20 @@
 package com.pcl.pos.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import jdk.swing.interop.SwingInterOpUtils;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class SignUpFormController {
     public TextField txtUsrNme;
@@ -13,9 +24,41 @@ public class SignUpFormController {
     public PasswordField txtCnfrmPwd;
     public AnchorPane cntxt;
 
-    public void signupOnAction(ActionEvent actionEvent) {
+    public void signupOnAction(ActionEvent actionEvent) throws ClassNotFoundException, SQLException {
+       String userName =txtUsrNme.getText();
+       String email=txtEml.getText();
+       String contact=txtCntctNmbr.getText();
+       String password=txtPwd.getText();
+       String pwdConfirmation=txtCnfrmPwd.getText();
+       String userId="USER-001";
+       String roleId="AD-001";
+
+       Class.forName("com.mysql.cj.jdbc.Driver");
+      Connection connection= DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/nextstack_pos","root","1234");
+      String sql="INSERT INTO user VALUES" +
+              "('"+userId+"','"+userName+"','"+password+"','"+email+"','"+contact+"','"+roleId+"')";
+        Statement statement = connection.createStatement();
+        int count=statement.executeUpdate(sql);
+        System.out.println(count);
+
+        //preapred
+
+        //LOAD DRIVER
+        //CREATE CONNECTION
+        //WRITE QUERY
+        //CREATE STATEMENT
+        //EXECUTE
     }
 
-    public void backToHomeOnAction(ActionEvent actionEvent) {
+    public void backToHomeOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("WelcomeForm");
+    }
+
+    public void setUi(String location) throws IOException {
+        Parent parent= FXMLLoader.load(getClass().getResource
+                ("/com/pcl/pos/view/"+location+".fxml"));
+        javafx.stage.Stage stage =(Stage)cntxt.getScene().getWindow();
+        stage.setScene(new Scene(parent));
+
     }
 }
--- src/module-info.java
+++ src/module-info.java
@@ -2,6 +2,9 @@ module POS {
     requires javafx.fxml;
     requires javafx.controls;
     requires javafx.graphics;
+    requires jdk.unsupported.desktop;
+    requires mysql.connector.j;
+    requires java.sql;
 
     opens com.pcl.pos.controller to javafx.fxml;
     opens com.pcl.pos.view to javafx.base;
